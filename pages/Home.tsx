deaad1d5755d7598dcc25627f5e540d2e68f4158
Raja Ahmed Affandi <RajaAhmedEffandi@Rajas-MacBook-Pro.local>
2022-12-23 01:56:08 +0500

    pagination added

--- pages/Home.tsx
+++ pages/Home.tsx
@@ -2,15 +2,15 @@ import React from 'react'
 
 const Characters = ({ name, img , status}: {name: string , img : string , status: string}) => {
   return (
-    <div className="bg-gray-200 rounded-md shadow-lg">
-      <div>
-      <img src={img} alt="" />
+    <div className="bg-gray-400 my-2 rounded-md shadow-lg">
+      <div className='flex justify-center '>
+      <img className='rounded-[100%] shadow-2xl h-[300px] w-[300px]' src={img} alt="" />
       </div>
-    <div className='flex justify-center'>{name}</div>
+    <div className='flex justify-center bg-white py-2 shadow-2xl mt-1'>{name}</div>
     <div>
       {status.includes('Alive')
         ? <div className='bg-green-200 flex justify-center py-4'>{status}</div>
-        : <div className='bg-red-300 py-4 flex justify-center '>{status}</div>
+        : <div className='bg-red-300 shadow-2xl py-4 flex justify-center '>{status}</div>
       }
     </div>
   </div>
--- pages/Pagination.tsx
+++ pages/Pagination.tsx
@@ -3,13 +3,13 @@ import React from 'react'
 const Pagination = ({ nextPage, prevPage, goToPage, pages } : {nextPage: any , prevPage : any , goToPage: any , pages : any}) => {
     let pageButtons = []
     for (let i = 1; i <= pages; i++) {
-    pageButtons.push(<button className='bg-white rounded-md shadow-lg w-6' key={i} onClick={() => goToPage(i)}>{i}</button>)
+    pageButtons.push(<button className='bg-wheat rounded-md shadow-lg w-6' key={i} onClick={() => goToPage(i)}>{i}</button>)
   }
   return ( 
-    <div className='max-w-[900px] border-2 border-black border-dotted space-x-2 rounded-md shadow-lg bg-gray-200 mx-auto'>
-      {prevPage && (<button className='bg-white px-2 rounded-md shadow-lg' onClick={prevPage}>Previous</button>)}
+    <div className='max-w-[900px] border-4 border-black  gap-3 flex flex-wrap py-2 rounded-md shadow-2xl bg-gray-200 mx-auto'>
+      {prevPage && (<button className='bg-white px-2 py-1 rounded-md shadow-lg' onClick={prevPage}>Previous</button>)}
       {pageButtons}
-      {nextPage && (<button className='bg-white px-2 rounded-md shadow-lg' onClick={nextPage}>Next</button>)}
+      {nextPage && (<button className='bg-white px-2 py-1 rounded-md shadow-lg' onClick={nextPage}>Next</button>)}
     </div>
   )
 }
