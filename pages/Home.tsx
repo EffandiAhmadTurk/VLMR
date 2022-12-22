9323f110416275d7bf4a3b096af3bccec1a69b8b
Raja Ahmed Affandi <RajaAhmedEffandi@Rajas-MacBook-Pro.local>
2022-12-23 01:18:01 +0500

    pagination feature

--- pages/Home.tsx
+++ pages/Home.tsx
@@ -1,12 +1,18 @@
 import React from 'react'
 
-const Characters = ({ name, img }: {name: string , img : string}) => {
+const Characters = ({ name, img , status}: {name: string , img : string , status: string}) => {
   return (
     <div className="bg-gray-200 rounded-md shadow-lg">
       <div>
       <img src={img} alt="" />
       </div>
-    <div>{name}</div>
+    <div className='flex justify-center'>{name}</div>
+    <div>
+      {status.includes('Alive')
+        ? <div className='bg-green-200 flex justify-center py-4'>{status}</div>
+        : <div className='bg-red-300 py-4 flex justify-center '>{status}</div>
+      }
+    </div>
   </div>
   )
 }
